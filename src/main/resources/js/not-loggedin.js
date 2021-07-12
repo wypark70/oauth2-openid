9415f0ef0ff684a757776338855f2c6ccc2e8e28
W.Y.Park <wypark70@gmail.com>
2021-07-13 05:53:25 +0900

    redirect auth login

--- src/main/resources/js/not-loggedin.js
+++ src/main/resources/js/not-loggedin.js
@@ -1,14 +1,17 @@
 (function ($) {
-    AJS.$.ajax({
-        url: "/rest/gadget/1.0/currentUser",
-        type: 'get',
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-            console.log("data:", JSON.stringify(data, null, "\t"));
-        },
-        error:function(request,status,error){
-          console.log(">>>>>>>>>>>>>>> not logged in <<<<<<<<<<<<<<<<<<<<<");
-        }
-    });
+    console.log("window.location.href:", window.location.href);
+    if (window.location.href.indexOf("/login.jsp") === -1) {
+        AJS.$.ajax({
+            url: "/rest/gadget/1.0/currentUser",
+            type: 'get',
+            dataType: 'json',
+            async: false,
+            success: function (data) {
+                console.log("data:", JSON.stringify(data, null, "\t"));
+            },
+            error: function (request, status, error) {
+                window.location.href = "/plugins/servlet/oauth-login";
+            }
+        });
+    }
 })(AJS.$ || jQuery);
